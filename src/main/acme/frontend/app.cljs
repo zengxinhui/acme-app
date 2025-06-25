fe41a887d67113744ca4d831b880804eec532e80
Xinhui Zeng <zengxh@gmail.com>
2025-06-25 01:06:24 -0400

    Bring in reagent and update example

--- src/main/acme/frontend/app.cljs
+++ src/main/acme/frontend/app.cljs
@@ -1,5 +1,18 @@
-(ns acme.frontend.app)
+(ns acme.frontend.app
+  (:require [clojure.string :as str]
+            [reagent.core :as r]
+            [reagent.dom.client :as rdomc]))
 
-(defn init []
-  (js/alert "Hello World")
-  (println "Hello World"))
+(defonce timer (r/atom (js/Date.)))
+
+(defonce time-updater (js/setInterval
+                       #(reset! timer (js/Date.)) 1000))
+
+(defn root []
+  (println "Hello World")
+  [:div "Hello World! The time now is " (-> @timer .toTimeString (str/split " ") first)])
+
+(defonce react-root (delay (rdomc/create-root (.getElementById js/document "root"))))
+
+(defn ^:export init []
+  (rdomc/render @react-root [root]))
